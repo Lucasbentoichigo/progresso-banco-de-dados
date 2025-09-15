b49b4f4a77f69e44aa84e189ffbd83006d5550c9
lucas <lucastavaresbento@gmail.com>
2025-09-15 10:52:29 -0300

    feat: Finaliza selects

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -1,3 +1,11 @@
 SELECT * FROM artistas
 
 SELECT COUNT(*) AS total_artistas FROM artistas;
+
+SELECT nome, musical_mais_ouvida FROM artistas;
+
+SELECT nome, musical_mais_ouvida FROM artistas
+WHERE pais_origem = 'Estados Unidos';
+
+SELECT nome, musical_mais_ouvida FROM artistas
+WHERE id = 3;
