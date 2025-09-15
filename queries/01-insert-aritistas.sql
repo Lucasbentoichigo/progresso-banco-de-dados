d15007f2d857ea2c43e1b7dc877af458a19e47fa
lucas <lucastavaresbento@gmail.com>
2025-09-15 10:45:01 -0300

    feat: Cria o arquivo para selects

--- queries/01-insert-aritistas.sql
+++ queries/01-insert-aritistas.sql
@@ -12,4 +12,12 @@ CREATE TABLE artistas (
 
 INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_origem, ano_inicio, musical_mais_ouvida, ativo)
 VALUES
-('Coldplay', 'Banda', 4, 'Rock', 'Reino Unido', 1996, 'Yellow', TRUE);
+('Adele', 'Cantora', 1, 'Pop', 'Reino Unido', 2006, 'Hello', TRUE),
+('Beyoncé', 'Cantora', 1, 'R&B', 'Estados Unidos', 1997, 'Single Ladies', TRUE),
+('Imagine Dragons', 'Banda', 4, 'Rock', 'Estados Unidos', 2008, 'Radioactive', TRUE),
+('Dua Lipa', 'Cantora', 1, 'Pop', 'Reino Unido', 2015, 'Levitating', TRUE),
+('Bruno Mars', 'Cantor', 1, 'Pop/R&B', 'Estados Unidos', 2004, 'Uptown Funk', TRUE),
+('Maroon 5', 'Banda', 5, 'Pop Rock', 'Estados Unidos', 1994, 'Sugar', TRUE),
+('Billie Eilish', 'Cantora', 1, 'Pop', 'Estados Unidos', 2015, 'Bad Guy', TRUE),
+('The Weeknd', 'Cantor', 1, 'R&B/Pop', 'Canadá', 2010, 'Blinding Lights', TRUE);
+
